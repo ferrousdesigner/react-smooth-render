1e7c693cf0a797b3a1861837eb1c0a7eeb01b469
Faraz Khan <faraaz.6nov@gmail.com>
2019-04-20 12:33:14 +0530

    Fixed initial hiding

---
+++ README.md
@@ -0,0 +1,50 @@
+# React Smooth Render
+
+
+
+Mount & unmount react components visually smooth using **React Fragments** without leaving any footprints.
+
+Demo: [https://ferrousdesigner.github.io/react-smooth-render/](https://ferrousdesigner.github.io/multigradient/)
+
+## Props
+
+|      Props          |Type                       | Default Value               |Description
+|----------------|-------------------------------|-----------------------------|-------
+|hidden|Boolean(Required)|false| Describes the visibility of the component
+|initiallyHidden |Boolean|false| Describes the initial visibility of the component   
+|timing          |Number |250| Animation duration in milliseconds
+|children|Node(Required)| undefined
+
+## Installation
+~~~
+npm install --save react-smooth-render
+~~~
+
+## Usage
+~~~
+import  SmoothRender  from  'react-smooth-render';
+
+class App extends Component {
+  state =  {
+    hidden: false // State varible which is used to toggle the visibility on SmoothRender
+  }
+	render() {
+		const { hidden } = this.state
+		return (
+			<div>
+				{/* This button toggles the state */}
+		        <button 
+			        onClick={() => this.setState({ hidden: !hidden })}>
+			        Toggle
+			     </button>
+				<SmoothRender hidden={hidden} timing={350}>
+			        <h1>This will mount and unmount smoothly.</h1>
+				</SmoothRender>
+			</div>
+		);
+	}
+}
+
+export default App;
+~~~
+
---
+++ src/index.js
@@ -0,0 +1,104 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import StyleMaker from 'stylemaker';
+const style = timing => `
+.react-smooth-render.hide {
+    animation: hide ${timing}ms forwards;
+}
+.react-smooth-render.reveal {
+	animation: reveal ${timing}ms forwards;
+}
+.react-smooth-render.invisible {
+    display: none;
+}
+@keyframes hide {
+    0 {
+      opacity: 1;
+    }
+    90% {
+      opacity: 0;
+    }
+    100% {
+			opacity: 0;
+			display: none;
+    }
+}
+@keyframes reveal {
+	0% {
+		opacity: 0;
+	}
+	10% {
+		opacity: 0;
+	}
+	100% {
+		opacity: 1;
+	}
+}
+`;
+// Add all the styles to the head
+export default class SmoothRender extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			initialClass: props.initiallyHidden ? 'initially-invisible' : '',
+			anchor: !(props.initiallyHidden || props.hidden),
+		};
+	}
+	componentWillMount() {
+		const { timing } = this.props;
+		if (!document.getElementById('react-smooth-render-styles')) {
+			StyleMaker(style(timing), 'react-smooth-render-styles', true);
+		}
+	}
+	componentWillReceiveProps(nextProps) {
+		const { initialClass, anchor } = this.state
+		const { hidden, timing } = nextProps
+		if (initialClass === 'initially-invisible') {
+			if (hidden) {
+				this.setState({ initialClass: '' });
+				setTimeout(() => {
+					this.setState({ anchor: false });
+				}, timing);
+			} else {
+				this.setState({ initialClass: '', anchor: true });
+			}
+		} else if ((hidden && anchor) || (!hidden && anchor)) {
+			setTimeout(() => {
+				this.setState({ anchor: false });
+			}, timing);
+		} else if (!hidden && !anchor) {
+			this.setState({ anchor: true });
+		}
+	}
+	render() {
+		const { children, hidden, initiallyHidden } = this.props;
+		const { anchor, initialClass } = this.state;
+		return (
+			<Fragment>
+				{anchor && (
+					<div
+						className={
+							'react-smooth-render' +
+							(initiallyHidden ? ` ${initialClass}` : '') +
+							(hidden ? ' hide' : ' reveal')
+						}
+					>
+						{children}
+					</div>
+				)}
+			</Fragment>
+		);
+	}
+}
+SmoothRender.defaultProps = {
+	children: null,
+	hidden: false,
+	initiallyHidden: false,
+	timing: 250,
+};
+SmoothRender.propTypes = {
+	children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+	hidden: PropTypes.bool.isRequired,
+	initiallyHidden: PropTypes.bool,
+	timing: PropTypes.number,
+};
